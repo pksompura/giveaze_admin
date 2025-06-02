8f5aec4359b52c26ef0306eddd9051c469afa397
diva-sl <divakaran.sivasamy@gmail.com>
2025-06-02 18:02:01 +0530

    jnkasaks

--- src/redux/services/axiosBaseQuery.js
+++ src/redux/services/axiosBaseQuery.js
@@ -4,7 +4,7 @@ import { setAccessToken } from "../slices/authSlice";
 const axiosInstance = axios.create({
   // baseURL: `https://devaseva.onrender.com/api`,
   // baseURL: `http://localhost:5001/api`,
-  baseURL: `http://88.222.214.214/api`,
+  baseURL: `https://88.222.214.214/api`,
   // baseURL: `https://temple-donation.onrender.com/api`,
   headers: {
     accept: `application/json`,
